8b1a9f5c4bc63fcec1dc1ca78bcda58f9c987ed8
Fedor <fodof179@gmail.com>
2022-12-11 11:37:37 +0300

    Added file reading

---
+++ src/FilesReader.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class FilesReader {
+    private String root;
+    private Graph filesGraph;
+
+    public FilesReader() {
+        System.out.println("Введите путь к корневому каталогу.");
+        Scanner in = new Scanner(System.in);
+        root = in.next();
+        while (!new File(root).isDirectory()) {
+            System.out.println("Нет такого каталога!");
+            System.out.println("Введите путь к корневому каталогу.");
+            root = in.next();
+        }
+    }
+
+    public void createGraph() {
+        filesGraph = new Graph();
+        try {
+            createGraphDFS(new File(root));
+        } catch (FileNotFoundException e) {
+            System.out.println("Проблемы с чтением файлов!");
+        }
+        filesGraph.getList();
+    }
+
+    public void print() {
+        for (int i = 0; i < filesGraph.result.length; ++i) {
+            for (int j = 0; j < filesGraph.result[i].size(); ++j) {
+                System.out.println(filesGraph.result[i].get(j));
+            }
+        }
+    }
+
+    private void createGraphDFS(File path) throws FileNotFoundException {
+        if (path.listFiles() == null) {
+            return;
+        }
+        for (File item : path.listFiles()) {
+            if (item.isDirectory()) {
+                createGraphDFS(item);
+            } else {
+                BufferedReader reader = new BufferedReader(new FileReader(item.toString()));
+                String line;
+                try {
+                    while ((line = reader.readLine()) != null) {
+                        filesGraph.add(item.getAbsolutePath(), line.split(" ")[1]);
+                    }
+                } catch (Exception e) {
+                    System.out.println("Проблема c содержимым файла!");
+                }
+            }
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Graph filesGraph = new Graph();
-        filesGraph.add("a");
-        filesGraph.add("aa", "a");
-        filesGraph.add("aaa", "aa");
-        filesGraph.add("b", "a");
-        filesGraph.add("bb", "b");
-        filesGraph.add("aaa", "bb");
-        filesGraph.add("aaa", "a");
-        filesGraph.getList();
-        for (int i = 0; i < filesGraph.result.length; ++i) {
-            for (int j = 0; j < filesGraph.result[i].size(); ++j) {
-                System.out.println(filesGraph.result[i].get(j));
-            }
-        }
+        FilesReader cur = new FilesReader();
+        cur.createGraph();
+        cur.print();
     }
 }
